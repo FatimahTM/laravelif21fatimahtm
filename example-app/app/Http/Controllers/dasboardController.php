098928bad80fb510d68f39d7110c37b3bebee614
FatimahTM <161033481+FatimahTM@users.noreply.github.com>
2024-06-11 18:42:43 +0700

    p
    p

---
+++ example-app/app/Http/Controllers/dasboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $mahasiswaprodi = DB::select("SELECT prodis.nama, COUNT(*) AS jumlah FROM mahasiswas JOIN prodis ON mahasiswas.prodi_id = prodis.id GROUP BY prodis.nama");
+        
+        $mahasiswajk = DB::select("SELECT prodis.nama, 
+            SUM(CASE WHEN mahasiswas.jk = 'L' THEN 1 ELSE 0 END) AS laki, 
+            SUM(CASE WHEN mahasiswas.jk = 'P' THEN 1 ELSE 0 END) AS perempuan 
+            FROM mahasiswas
+            JOIN prodis ON mahasiswas.prodi_id = prodis.id
+            GROUP BY prodis.nama;");
+        
+        return view('dashboard')->with('mahasiswaprodi', $mahasiswaprodi)->with('mahasiswajk', $mahasiswajk);
+    }
+}
+
